c968104edfe2a3206317da49e7f4f04723df7b57
ozivarol <oguzhanvaroll43@gmail.com>
2021-12-28 05:28:44 +0300

    Update api.test.js

--- src/api.test.js
+++ src/api.test.js
@@ -98,8 +98,8 @@ const app = require('../src/app');
         .post('/api/filter')
         .send({
           startDate: '2016-01-26',
-          endDate: '2018-02-02',
-          minCount: 2700,
+          endDate: '2017-02-02',
+          minCount: 1000,
           maxCount: 3000
         })
         .then(response => {
